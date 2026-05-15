a3ec0c23c11ded7c17e87cd2e42ce62f50effd59
zonorin <kongsonorin22@gmail.com>
2026-05-15 15:29:05 +0700

    Add user route and optional post slug route

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::get('/about', function () {
     return view('about');
+});
+
+Route::get('/user/{id}', function ($id) {
+    return "User ID: $id";
+});
+
+Route::get('/post/{slug?}', function ($slug = 'default-post') {
+    return "Post Slug: $slug";
 });
