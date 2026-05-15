cee685b9f5d37c48942b6e695e43b0989b6d7ba5
zonorin <kongsonorin22@gmail.com>
2026-05-15 14:30:48 +0700

    Add about page route and view

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About page</title>
+</head>
+<body>
+    <h1>About Us</h1>
+    <p>This is the about page content.</p>
+</body>
+</html>
